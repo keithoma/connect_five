f725c58a56bb56b59d2f1a59f853ddf7dda2065d
Christian Parpart <christian@parpart.family>
2021-05-21 13:43:50 +0200

    added cmake and improved some code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <tuple>
+#include <list>
 #include <unistd.h>
 #include <vector>
 
@@ -24,10 +25,21 @@ class Board {
         {
         }
 
-    void print_board();
-    State check_for_win();
+	State at(int _row, int _col) const
+	{
+		return board.at(_row).at(_col);
+	}
+
+	void set(int _row, int _col, State _value)
+	{
+		board.at(_row).at(_col) = _value;
+	}
+
+    void print_board() const;
+    State check_for_win() const;
 
     void add_and_animate(int _column, State _state);
+
 
   private:
     unsigned short int width;
@@ -37,7 +49,7 @@ class Board {
     vector<vector<State>> transpose();
 };
 
-void Board::print_board() {
+void Board::print_board() const {
     // header
     cout << "\033[2J\033[1;1H"; // clears screen
     cout << "\n";
@@ -124,36 +136,154 @@ vector<vector<State>> Board::transpose() {
     return trans;
 }
 
-State Board::check_for_win() {
-    for (vector<State> column : board) {
-        State win = check_vector(column);
-        switch (win) {
-            case State::blue : return State::blue;
-            case State::red : return State::red;
-        }
-    }
+constexpr int WIN_COUNT = 5;
+
+State Board::check_for_win() const {
+	// 1.) check for horizontal win
+	for (size_t row = 0; row < BOARD_HEIGHT; ++row)
+	{
+		int red = 0;
+		int blue = 0;
+		State lastState = State::empty;
+		for (size_t col = 0; col < BOARD_WIDTH - WIN_COUNT; ++col)
+		{
+			State const currentState = at(row, col);
+			if (currentState != lastState)
+			{
+				red = 0;
+				blue = 0;
+				lastState = currentState;
+			}
+			switch (currentState)
+			{
+				case State::red:
+					red++;
+					break;
+				case State::blue:
+					blue++;
+					break;
+				case State::empty:
+					break;
+			}
+			if (red == WIN_COUNT)
+				return State::red;
+			if (blue == WIN_COUNT)
+				return State::blue;
+		}
+	}
+
+	// 2.) check for vertical win
+	for (size_t col = 0; col < BOARD_WIDTH; ++col)
+	{
+		for (size_t row = 0; row < BOARD_WIDTH - WIN_COUNT; ++row)
+		{
+			// TODO: check downwards
+		}
+	}
+
+	// 3.) check for diagonal win of the upper triangle
+	// TODO
+	// for (size_t diag = 0; diag < min(BOARD_WIDTH, BOARD_HEIGHT) - WIN_COUNT; ++diag)
+	// {
+	// }
+    //
+	// for (size_t col = 0; col < BOARD_WIDTH - WIN_COUNT; ++col)
+	// {
+	// 	int red = 0;
+	// 	int blue = 0;
+	// 	for (size_t row = 0; row < BOARD_HEIGHT - WIN_COUNT; ++row)
+	// 	{
+	// 		State lastStae = State::empty;
+	// 		State const currentState = at(row, col);
+	// 		if (currentState != lastState)
+	// 		{
+	// 			red = 0;
+	// 			blue = 0;
+	// 			lastState = currentState;
+	// 		}
+	// 	}
+	// }
+
+	// 4.) check for diagonal win of the lower triangle
 }
 
-Board game_loop(Board board) {
-    int row;
+enum class Player { Red, Blue };
+string to_string(Player _player) {
+	switch (_player) {
+		case Player::Red: return "RED";
+		case Player::Blue: return "BLUE";
+	}
+	throw std::invalid_argument("Player");
+}
 
+State to_state(Player _player)
+{
+	switch (_player) {
+		case Player::Red: return State::red;
+		case Player::Blue: return State::blue;
+	}
+	throw std::invalid_argument("Player");
+}
+
+Board player_turn(Board board, Player _player) {
     board.print_board();
     cout << "\n";
-    cout << "It's GREEN's player's turn. On which row do you want to put a stone?" << endl;
+    cout << "It's " << to_string(_player) << "'s player's turn. On which row do you want to put a stone?" << endl;
+
+    int row;
     cin >> row;
-    board.add_and_animate(row - 1, State::blue);
+
+    board.add_and_animate(row - 1, to_state(_player));
     board.print_board();
-    cout << "\n";
-    cout << "It's red player's turn. On which row do you want to put a stone?\n>>";
-    cin >> row;
-    board.add_and_animate(row - 1, State::red);
+
     return board;
 }
 
-int main() {
-    Board board(BOARD_WIDTH, BOARD_HEIGHT);
-    while (true) {
-        board = game_loop(board);
-    }
+Board game(Board const& _board)
+{
+	Board board = _board; // python: baord = copy(_board);
+	board.print_board();
+
+	Board const& bref = _board;
+	bref.print_board();
+
+	Board const* pref = &_board;
+	pref->print_board();
+
+
+	for (Player const player: {Player::Red, Player::Blue})
+	{
+		board = player_turn(board, player);
+		switch (board.check_for_win())
+		{
+			case State::empty:
+				return game(board);
+			default:
+				break;
+		}
+	}
+	return board;
+}
+
+int main()
+{
+	auto const resultBoard = game(Board(BOARD_WIDTH, BOARD_HEIGHT));
+
+	switch (resultBoard.check_for_win())
+	{
+		case State::red:
+			cout << "Team RED has won!\n";
+			break;
+		case State::blue:
+			cout << "Team BLUE has won!\n";
+			break;
+		case State::empty:
+			cout << "HOLY SHIT YOU BROKE THE SYSTEM\n";
+			break;
+	}
+
+    // while (true) {
+    //     board = game_loop(board);
+    // }
     return 0;
 }
