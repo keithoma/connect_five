a149a4c7e3429eedd12f44834f8dfea67b03f787
Kei <thomakmj@gmail.com>
2021-05-23 17:34:00 +0200

    save commit

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 constexpr unsigned short int BOARD_WIDTH = 12;
 constexpr unsigned short int BOARD_HEIGHT = 8;
 
-enum State { empty, blue, red };
+enum State { empty, green, red };
 
 class Board {
   public:
@@ -49,30 +49,21 @@ class Board {
     unsigned short int height;
 
     State check_vector(vector<State>);
-    vector<vector<State>> transpose();
 };
 
 void Board::print_board() const {
+    // clear screen
+    cout << "\033[2J\033[1;1H" << "\n";
+
     // header
-    cout << "\033[2J\033[1;1H"; // clears screen
-    cout << "\n";
-    // centralize
-    // C O N N E C T   F I V E   <---   23 char
-    // 80 - 23 = 57
-    // 29 and 28 spaces
-    cout << "                             C O N N E C T   F I V E                            ";
-    cout << "\n\n\n";
+    cout << "                             C O N N E C T   F I V E                            \n\n\n";
 
     // board
-    // 12 columns   => 80 / 12 = 6.666
-    //              => 12 * 5 = 60
-    // Therefore, margin of 12 on each side
-    // each column gets 4 spaces inbetween
     for (int row = 0; row < height; ++row) {
     cout << "            ";
         for (int column = 0; column < width; ++column) {
             switch (board[row][column]) {
-                case State::blue:
+                case State::green:
                     cout << "\x1b[32mX\x1b[0m";
                     break;
                 case State::red:
@@ -86,17 +77,15 @@ void Board::print_board() const {
         }
         cout << "\n";
     }
-    cout << "           [1]  [2]  [3]  [4]  [5]  [6]  [7]  [8]  [9]  [10] [11] [12]";
-    cout << "\n\n\n";
+    cout << "           [1]  [2]  [3]  [4]  [5]  [6]  [7]  [8]  [9]  [10] [11] [12]\n\n\n";
 }
 
 void Board::add_and_animate(int _column, State _state) {
     for (int row = 0; row < height; ++row) {
-        if (board[row][_column] == State::empty) {
-            board[row][_column] = _state;
-            if (row >= 1) {
-                board[row - 1][_column] = State::empty;
-            }
+        if (at(row, _column) == State::empty) {
+            set(row, _column, _state);
+            if (row >= 1)
+				set(row - 1, _column, State::empty);;
         } else {
             break;
         }
@@ -105,125 +94,25 @@ void Board::add_and_animate(int _column, State _state) {
     }
 }
 
-// should use negative numbers instead of two ints
-State Board::check_vector(vector<State> vec) {
-    int points_blue = 0;
-    int points_red  = 0;
-    for (State state : vec) {
-        if (state == State::blue) {
-            points_blue++;
-            points_red = 0;
-        } else if (state == State::red) {
-            points_red++;
-            points_blue = 0;
-        } else {
-            points_blue = 0;
-            points_red = 0;
-        }
-
-        if (points_blue == 5) {
-            return State::blue;
-        } else if (points_red == 5) {
-            return State::red;
-        }
-    }
-    return State::empty;
-}
-
-// this should only give me a reference? pointer? but not create a new board.
-vector<vector<State>> Board::transpose() {
-    vector<vector<State>> trans (height, vector<State>(width, State::empty));
-    for (int i = 0; i < width; ++i)
-        for (int j = 0; j < height; ++j)
-            trans[j][i] = board[i][j];
-    return trans;
-}
-
-constexpr int WIN_COUNT = 5;
-
 State Board::check_for_win() const {
-	// 1.) check for horizontal win
-	for (size_t row = 0; row < BOARD_HEIGHT; ++row)
-	{
-		int red = 0;
-		int blue = 0;
-		State lastState = State::empty;
-		for (size_t col = 0; col < BOARD_WIDTH - WIN_COUNT; ++col)
-		{
-			State const currentState = at(row, col);
-			if (currentState != lastState)
-			{
-				red = 0;
-				blue = 0;
-				lastState = currentState;
-			}
-			switch (currentState)
-			{
-				case State::red:
-					red++;
-					break;
-				case State::blue:
-					blue++;
-					break;
-				case State::empty:
-					break;
-			}
-			if (red == WIN_COUNT)
-				return State::red;
-			if (blue == WIN_COUNT)
-				return State::blue;
-		}
-	}
 
-	// 2.) check for vertical win
-	for (size_t col = 0; col < BOARD_WIDTH; ++col)
-	{
-		for (size_t row = 0; row < BOARD_WIDTH - WIN_COUNT; ++row)
-		{
-			// TODO: check downwards
-		}
-	}
-
-	// 3.) check for diagonal win of the upper triangle
-	// TODO
-	// for (size_t diag = 0; diag < min(BOARD_WIDTH, BOARD_HEIGHT) - WIN_COUNT; ++diag)
-	// {
-	// }
-    //
-	// for (size_t col = 0; col < BOARD_WIDTH - WIN_COUNT; ++col)
-	// {
-	// 	int red = 0;
-	// 	int blue = 0;
-	// 	for (size_t row = 0; row < BOARD_HEIGHT - WIN_COUNT; ++row)
-	// 	{
-	// 		State lastStae = State::empty;
-	// 		State const currentState = at(row, col);
-	// 		if (currentState != lastState)
-	// 		{
-	// 			red = 0;
-	// 			blue = 0;
-	// 			lastState = currentState;
-	// 		}
-	// 	}
-	// }
-
-	// 4.) check for diagonal win of the lower triangle
 }
 
-enum class Player { Red, Blue };
+enum class Player { green, red };
+
 string to_string(Player _player) {
 	switch (_player) {
-		case Player::Red: return "RED";
-		case Player::Blue: return "BLUE";
+		case Player::green: return "\x1b[32mGREEN\x1b[0m";
+		case Player::red: return "\x1b[31mRED\x1b[0m";
 	}
 	throw std::invalid_argument("Player");
 }
 
-State to_state(Player _player)
-{
+State to_state(Player _player) {
 	switch (_player) {
-		case Player::Red: return State::red;
-		case Player::Blue: return State::blue;
+		case Player::green: return State::green;
+		case Player::red: return State::red;
+
 	}
 	throw std::invalid_argument("Player");
 }
@@ -254,7 +143,7 @@ Board game(Board const& _board)
 	pref->print_board();
 
 
-	for (Player const player: {Player::Red, Player::Blue})
+	for (Player const player: {Player::green, Player::red})
 	{
 		board = player_turn(board, player);
 		switch (board.check_for_win())
@@ -277,7 +166,7 @@ int main()
 		case State::red:
 			cout << "Team RED has won!\n";
 			break;
-		case State::blue:
+		case State::green:
 			cout << "Team BLUE has won!\n";
 			break;
 		case State::empty:
